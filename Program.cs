354d9c18a30b5987018c20ac859dfb3f8e977044
KickButovsk1 <144268614+KickButovsk1@users.noreply.github.com>
2023-11-30 22:17:48 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int colvo = 0;
+            string[] array = new string[6] { "Привет", "Мама", "Россия", "Я люблю Россию", "Достижение", "Библиотека" };
+            int m = 6;
+            int w = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                w = array[i].Length;
+                if (w > m)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+    }
+}
